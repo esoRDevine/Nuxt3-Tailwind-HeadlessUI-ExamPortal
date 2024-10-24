11429a0dfe7f2955e9d086bf0f69f29beba7b887
Lucas <lucas@afrigis.co.za>
2024-10-24 14:12:42 +0200

    added  the login page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
       <TheHeader />
+      <NuxtPage />
       <TheFooter />
     </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
+    ssr: false, // Disable ssr, by default its enabled
   modules: ['nuxt-headlessui', '@nuxtjs/tailwindcss'],
   headlessui: {
     prefix: 'Headless'
